c2fd2420ed3fc8ddc949cb429fc9bc7e3b577b95
Mamunur Hossen <mamunur_hossen@yahoo.com>
2015-07-30 16:51:32 +0600

    Update sample.php

--- excel-lib/reader/sample.php
+++ excel-lib/reader/sample.php
@@ -1,31 +1,28 @@
 <?php 
-   $mtime = microtime(); 
-   $mtime = explode(" ",$mtime); 
-   $mtime = $mtime[1] + $mtime[0]; 
-   $starttime = $mtime; 
-?> 
-
-<?php
-
-	require_once "simplexlsx.class.php";
+	header("Cache-Control: no-store, no-cache, must-revalidate, max-age=0");
+	header("Cache-Control: post-check=0, pre-check=0", false);
+	header("Pragma: no-cache");
 	
-	$fileName = '../data.xlsx';
+	error_reporting(-1);
+	ini_set('display_errors', 1);
+	ini_set('memory_limit', '-1');
 
-	$xlsx = new SimpleXLSX($fileName);
+	//Note: You need to install the following things to run the xlsx script
+	//sudo apt-get install php5-mcrypt php5-curl php5-gd php5-xsl zziplib-bin
 	
+ 	require_once "excel-lib/reader/simplexlsx.class.php";
+	
+	//$fileName = '../data.xlsx';
+	$fileName = "teacher.xlsx";
+
+	$xlsx = new SimpleXLSX($fileName, false, true);
+
 	list($cols,) = $xlsx->dimension();
-	
+
+	echo count($xlsx->rows());
+	$count = 0;
 	foreach( $xlsx->rows() as $k => $r) {
 		echo $k." => ".$r[1];
+		$count++;
+		if($count > 2) break;
 	}
-
-?>
-
-<?php 
-   $mtime = microtime(); 
-   $mtime = explode(" ",$mtime); 
-   $mtime = $mtime[1] + $mtime[0]; 
-   $endtime = $mtime; 
-   $totaltime = ($endtime - $starttime); 
-   echo "This page was created in ".$totaltime." seconds"; 
-;?>
